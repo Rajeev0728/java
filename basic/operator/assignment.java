5838c258034d8cd48cc7375e9305e3dd6cf96d3e
Rajeev kumar <rejeev8252254931@gmail.com>
2026-05-19 21:29:45 +0530

    assignment operator program done

---
+++ basic/operator/assignment.java
@@ -0,0 +1,20 @@
+//assignment operator :-
+public class assignment {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6+a;
+        int c = 2-a;
+        int d = 3*a;
+        int e = 10%a; // modulo operator
+
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        // comparison operator 
+    System.out.println(6==6);
+    System.out.println(16>10);
+    System.out.println(5>10);
+    }
+}
