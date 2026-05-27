52b5f1e1e17be5660e4ecd0b90f630afc7cc4318
Rajeev kumar <rejeev8252254931@gmail.com>
2026-05-27 14:09:10 +0530

    basic string program done

---
+++ opeartor/string.java
@@ -0,0 +1,10 @@
+
+public class string{
+    public static void main (String[] args){
+        String name = "Rajeev";
+        System.out.print("The name is:");
+        System.out.println(name);
+        
+    }  
+ }
+       
